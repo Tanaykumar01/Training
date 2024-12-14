8bf0760cf12070723977ccf9b094b413fc630c41
Tanaykumar01 <tanayjhunjhunwala.official@gmail.com>
2024-12-14 17:10:33 +0530

    initialize backend with Express, MongoDB, and basic user and todo models

---
+++ Day 1/todo-backend/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// import routes
+
+
+// use routes
+
+
+export { app };
